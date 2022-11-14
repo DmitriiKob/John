b61c299e9c78cf128d84f6d62969bd97fc023711
Dmitrii Kobyliatskii <dmitriykob1002@gmail.com>
2022-11-14 11:57:51 -0800

    firstSiteVersion

--- ivan's site/script.js
+++ ivan's site/script.js
@@ -8,3 +8,8 @@ exit.addEventListener('click', () => {
     const dropDown = document.querySelector('.dropdown')
      dropDown.style.top = '-100%'
     })
+
+    // const screenWidth = window.screen
+    // const destop = document.querySelector('.destop')
+    
+    // destop.style.margin-inline == 
