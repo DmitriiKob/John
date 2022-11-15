0bdb25f4d05bb3fdb580d2dd4bc519658199f367
Dmitrii Kobyliatskii <dmitriykob1002@gmail.com>
2022-11-15 09:30:33 -0800

    final_site

--- ivan's site/script.js
+++ ivan's site/script.js
@@ -1,15 +1,15 @@
 const burger = document.querySelector('.burger')
 const exit = document.querySelector('.exit')
+const dropdownHeader = document.querySelector('.dropdownHeader')
 burger.addEventListener('click', () => {
 const dropDown = document.querySelector('.dropdown')
  dropDown.style.top = '0'
+ dropdownHeader.style.display = 'flex'
 })
 exit.addEventListener('click', () => {
     const dropDown = document.querySelector('.dropdown')
      dropDown.style.top = '-100%'
+     dropdownHeader.style.display = 'none'
+
     })
 
-    // const screenWidth = window.screen
-    // const destop = document.querySelector('.destop')
-    
-    // destop.style.margin-inline == 
